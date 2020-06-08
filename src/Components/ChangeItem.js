b30f636f80cd0070153542d0c320bd2c99b4ad75
hjw4595 <hongjung73@gmail.com>
2020-06-08 23:49:19 +0900

    implement : changeItem

--- src/Components/ChangeItem.js
+++ src/Components/ChangeItem.js
@@ -33,18 +33,6 @@ const CurrentChange = ({ todoItem, currentChange, changeItem}) => {
         const { target: { value } } = event;
         setChangeInput(value)
     }
-    //span -> 버튼태그 
-    //update 속성명이 너무 x  ->  이부분은 mobx 안쓰고 
-    //value 초기값으로 value값(todoitme) 
-    //프로젝트 명 정하기
-    //ui어떻게 하면 좋겠냐 물어보기 
-    //server -> 감을 못잡겠음 어떤의민지 local storge? // loading? 
-    //server -> api? 사이트 ? node 찾아봄 // 애니메이션 
-    //mobx를 쓸곳과 안쓸곳 구분하기
-    //훅을 따로 만들어서 내가 원하는식의 인풋 함수?
-    //파일정리하기 -> 프레젠테이션 
-    //파일 다 만들어서 index 파일 추가? 
-    //유닛테스트 ㅇㅈ? 필수 ㅇㅋ 
         return ( 
             <>
                     {todoItem.change ? 
